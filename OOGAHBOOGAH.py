9a017027ae915324fac07b3e43ae58d705bdb210
gautier.leffebvre@gmail.com <gautier.leffebvre@gmail.com>
2022-09-22 09:20:08 +0000

    OOGAHBOOGAH

--- OOGAHBOOGAH.py
+++ OOGAHBOOGAH.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
-azezaezaezaezae
+dbutils.fs.mount(
+    source = "wasbs://conteneuresgi19@blobesgi19.blob.core.windows.net/conteneuresgi19",
+    mount_point = "/mnt/blob-storage",
+    extra_configs = {"fs.azure.account.key.blobesgi19.blob.core.windows.net": 
+                     dbutils.secrets.get(scope = "secretscopedatabricks19",
+                                         key = "KgpvwCikFX5OpLj7bF+fRuvvB9J9+YaF2VhiZEdKg4lZ7+jB1ziA+OBXco8Tmm9fNIJFWqZ7+Xvr+AStp6lElA==")
+                    }
+)
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/blob-storage
+
+# COMMAND ----------
+
+dbutils.fs.unmount("/mnt/blob-storage")
